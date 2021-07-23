1b2a1ff0ba299ba7b3dfb043ec155fa8294eff07
Dan Biscup <dbiscup@hotmail.com>
2021-07-23 13:50:17 -0500

    dan

--- m3/Program.cs
+++ m3/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Blob;
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Threading.Tasks;
 
@@ -27,6 +29,16 @@ namespace m3
                             await PullAsync(strValue);
                             break;
                         }
+                    case "init":
+                        {
+                            await InitAsync(strValue);
+                            break;
+                        }
+                    case "push":
+                        {
+                            await PushAsync(strValue);
+                            break;
+                        }
                 }
 
             }
@@ -36,9 +48,95 @@ namespace m3
             }
         }
 
+        private static async Task InitAsync(string strValue)
+        {
+            Console.WriteLine("Init  " + strValue);
+
+            strValue                                                    = Clean(strValue);
+
+            try
+            {
+                CloudStorageAccount saAccount                           = CloudStorageAccount.Parse(CONNECTION_STRING);
+                CloudBlobClient bClient                                 = saAccount.CreateCloudBlobClient();
+
+                CloudBlobContainer container                            = bClient.GetContainerReference(strValue);
+
+                bool result                                             = await container.CreateIfNotExistsAsync();
+
+                if (result != true)
+                    throw new Exception("Name already exists please select a different name.");
+
+                Directory.CreateDirectory(strValue);
+
+                Console.WriteLine("Success! Please add your mods and do a m3 push " + strValue + "!");
+                Console.WriteLine("Happy moding!");
+
+                string strDir                                           = Directory.GetCurrentDirectory() + @"\" + strValue;
+                Process.Start("explorer.exe", strDir);
+
+            }
+            catch (Exception exError)
+            {
+                Console.WriteLine("Errors!");
+                Console.WriteLine(exError.Message);
+            }
+        }
+
+        private static async Task PushAsync(string strValue)
+        {
+            Console.WriteLine("Push  " + strValue);
+
+            strValue                                                    = Clean(strValue);
+
+            try
+            {
+                CloudStorageAccount saAccount                           = CloudStorageAccount.Parse(CONNECTION_STRING);
+                CloudBlobClient bClient                                 = saAccount.CreateCloudBlobClient();
+                var bcContainer                                         = bClient.GetContainerReference(strValue);
+                List<string> astrServerFiles                            = new List<string>();
+                BlobContinuationToken blobContinuationToken             = null;
+                var results                                             = await bcContainer.ListBlobsSegmentedAsync(null, blobContinuationToken);
+                blobContinuationToken                                   = results.ContinuationToken;
+                foreach (IListBlobItem item in results.Results)
+                {
+                    string strFileName                                  = Path.GetFileName(item.Uri.ToString());
+                    Console.WriteLine("Checking Server: " + strFileName);
+                    astrServerFiles.Add(strFileName);
+                }
+
+                string[] astrLocalFiles                                 = Directory.GetFiles(Directory.GetCurrentDirectory() + @"\" + strValue);
+
+                foreach(string strLocalFile in astrLocalFiles)
+                {
+                    if (!astrServerFiles.Contains(Path.GetFileName(strLocalFile))) //Need to upload
+                    {
+                        Console.WriteLine("Adding: " + Path.GetFileName(strLocalFile));
+                        var newBlob                                     = bcContainer.GetBlockBlobReference(Path.GetFileName(strLocalFile));
+                        await newBlob.UploadFromFileAsync(strLocalFile);
+                    }
+                }
+
+                //Delete what is not suposed to be there
+                foreach(string strServerFile in astrServerFiles)
+                {
+                    if (!Contains(astrLocalFiles, strServerFile))
+                    {
+                        Console.WriteLine("Removing: " + strServerFile);
+                        var newBlob                                     = bcContainer.GetBlockBlobReference(strServerFile);
+                        await newBlob.DeleteAsync();
+                    }
+                }
+            }
+            catch (Exception exError)
+            {
+                Console.WriteLine("Errors!");
+                Console.WriteLine(exError.Message);
+            }
+        }
+
         private static async Task PullAsync(string strValue)
         {
-            Console.WriteLine("pulling " + strValue);
+            Console.WriteLine("Pulling " + strValue);
 
             try
             {
@@ -54,23 +152,36 @@ namespace m3
                 if (!Directory.Exists(strBase + @"\.minecraft\mods"))
                     Directory.CreateDirectory(strBase + @"\.minecraft\mods");
 
-                string[] astrLocalFiles                                     = Directory.GetFiles(strBase + @"\.minecraft\mods");
+                string[] astrLocalFiles                                 = Directory.GetFiles(strBase + @"\.minecraft\mods");
+                List<string> astrServerFiles                            = new List<string>();
 
-                CloudStorageAccount saAccount                               = CloudStorageAccount.Parse(CONNECTION_STRING);
-                CloudBlobClient bClient                                     = saAccount.CreateCloudBlobClient();
-                var bcContainer                                             = bClient.GetContainerReference(strValue);
+                CloudStorageAccount saAccount                           = CloudStorageAccount.Parse(CONNECTION_STRING);
+                CloudBlobClient bClient                                 = saAccount.CreateCloudBlobClient();
+                var bcContainer                                         = bClient.GetContainerReference(strValue);
 
-                BlobContinuationToken blobContinuationToken                 = null;
-                var results                                                 = await bcContainer.ListBlobsSegmentedAsync(null, blobContinuationToken);
-                blobContinuationToken                                       = results.ContinuationToken;
+                BlobContinuationToken blobContinuationToken             = null;
+                var results                                             = await bcContainer.ListBlobsSegmentedAsync(null, blobContinuationToken);
+                blobContinuationToken                                   = results.ContinuationToken;
                 foreach (IListBlobItem item in results.Results)
                 {
-                    string strFileName = Path.GetFileName(item.Uri.ToString());
+                    string strFileName                                  = Path.GetFileName(item.Uri.ToString());
+                    astrServerFiles.Add(strFileName);
                     Console.WriteLine("Checking: " + strFileName);
 
                     if (!Contains(astrLocalFiles, strFileName))
                     {
+                        Console.WriteLine("Adding: " + strFileName);
                         await bcContainer.GetBlockBlobReference(strFileName).DownloadToFileAsync(strBase + @"\.minecraft\mods\" + strFileName, FileMode.Create);
+                    }
+                }
+
+                //Delete what is not suposed to be there
+                foreach(string strFile in astrLocalFiles)
+                {
+                    if (!astrServerFiles.Contains(Path.GetFileName(strFile)))
+                    {
+                        Console.WriteLine("Removing: " + strFile);
+                        File.Delete(strFile);
                     }
                 }
             }
@@ -104,11 +215,11 @@ namespace m3
 
         public static string Clean(string str)
         {
-            string strReturn = string.Empty;
+            string strReturn                                            = string.Empty;
 
             try
             {
-                strReturn                                               = str.Trim().ToLower();
+                strReturn                                               = str.Trim().ToLower().Replace(" ", "");
             }
             catch (Exception) { };
             return strReturn;
